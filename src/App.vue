ccf50896bc5daea0eb1dd4936802e83110cdbede
Alan Cordeiro <alamcordeiro@gmail.com>
2019-11-30 11:30:52 -0500

    Vue

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+html, body{ margin: 0}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background: #000;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="container h-100">
+    <div class="row align-items-center h-100">
+      <div class="col-8 mx-auto">
+        <div class="content p-4 bg-dark b-0">
+          <div class="mb-4">
+            <img width="200" src="../assets/img/espartas2.png" class />
+            <h3 class="mt-2">
+              <img width="140" src="../assets/img/sublogo.png" />
+              <img width="140" src="../assets/img/free-fire.png" />
+            </h3>
+            <img height="30" src="../assets/img/maranhao.png" />
+          </div>
+
+          <h3>Reserve sua vaga!!!</h3>
+          <h4>1º CAMPEONATO eSpartas DE FREE FIRE DO MARANHÃO</h4>
+
+          <p>Inscrição gratuita para os primeiros 500 jogadores</p>
+
+          <div class="col-md-12">
+            <form class="row">
+              <div class="col-12 col-sm pr-sm-0">
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Nome do Jogador"
+                  class="form-control"
+                />
+              </div>
+
+              <div class="col-12 col-sm">
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="E-mail"
+                  class="form-control"
+                />
+              </div>
+
+              <div class="col-12 col-sm-auto pl-sm-0">
+                <input type="button" name="commit" value="Enviar" class="btn btn-primary btn-block" />
+              </div>
+            </form>
+
+            <div v-if="countdown">
+              <h5>Faltam</h5>
+              <h5 class="timer">
+                {{ countdown }}
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.container {
+  color: #dadae0 !important;
+  font-weight: 100;
+}
+
+.content {
+  background-image: url(http://themes.pixiesquad.com/pixiefreak/twisting-nether/wp-content/uploads/2018/10/background-copy.jpg);
+}
+</style>
+
+
+<script>
+export default {
+  data() {
+    return {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      countdown: 0
+    };
+  },
+
+  beforeMount() {
+    window.setInterval(() => {
+      this.countdown = this.getCountdown("01/01/2020 05:00:00 PM");
+    }, 1000);
+  },
+
+  methods: {
+  
+       getCountdown(endDate) {
+        let days, hours, minutes, seconds;
+
+        endDate = new Date(endDate).getTime();
+
+        if (isNaN(endDate)) {
+          return false;
+        }
+
+        let startDate = new Date().getTime();
+
+        let timeRemaining = parseInt((endDate - startDate) / 1000);
+
+        if (timeRemaining < 0) {
+          return false;
+        }
+
+        days = parseInt(timeRemaining / 86400);
+        timeRemaining = timeRemaining % 86400;
+
+        hours = parseInt(timeRemaining / 3600);
+        timeRemaining = timeRemaining % 3600;
+
+        minutes = parseInt(timeRemaining / 60);
+        timeRemaining = timeRemaining % 60;
+
+        seconds = parseInt(timeRemaining);
+        days = parseInt(days, 10);
+
+        hours = hours < 10 ? "0" + hours : hours;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        return `${days} dias, ${hours} horas, ${minutes} minutos ${seconds} segundos`
+
+      }
+    
+  }
+};
+</script>
